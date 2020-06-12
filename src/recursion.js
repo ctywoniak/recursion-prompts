c0cb58a9ac2cab596fb35a4782ef82f2b33f67ad
ctywoniak <ctywoniak@gmail.com>
2020-06-12 08:39:24 -0700

    Recoyshun

--- src/recursion.js
+++ src/recursion.js
@@ -7,11 +7,18 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+    if(n === 0) return 1;
+    if(n < 0) return null;
+    return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    var result = array[0];
+    console.log(result)
+    if(result === undefined) return 0;
+    return result += sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
@@ -21,6 +28,10 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+    if(n === 0) return true;
+    if(n === 1) return false;
+    if(n < 0) return isEven(n + 2);
+    return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
@@ -32,6 +43,7 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+
 };
 
 // 7. Compute the exponent of a number.
